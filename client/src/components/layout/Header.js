f7cfe18e4aba14ec165ac94ab56877c76082c34f
Joel Samaniego Campos <jsamani1@mail.sfsu.edu>
2019-05-13 10:12:03 -1000

    css updates

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -6,22 +6,26 @@ const Header = () => {
   return (
     <header className="headerStyle">
       <span className="columnStyle">
-        <h1>CSC 667/867 HW #3 (React)</h1>
+        <h1 className="header-left-align">My Dashboard</h1>
         <Link className="linkStyle" to="/">
           Home
         </Link>
+        <i className="home icon white"></i>
         <span> | </span>
         <Link className="linkStyle" to="/breweries">
           Breweries
         </Link>
+        <i className="beer icon yellow"></i>
         <span> | </span>
         <Link className="linkStyle" to="/weather">
           Weather
         </Link>
+        <i className="sun icon gold"></i>
         <span> | </span>
         <Link className="linkStyle" to="/lookupmusicband">
           Lookup Music Band
         </Link>
+        <i className="headphones icon red"></i>
       </span>
       <span className='columnStyle'>
           <LocationData/>
