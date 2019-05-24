b66f755a03f4d5876b576cce701f546869ce0a46
Ilya Kopyl <ilya.kopyl@gmail.com>
2019-05-24 16:53:28 -0700

    Cleaning up a little bit

--- server/microservices/lookupmusicband.js
+++ server/microservices/lookupmusicband.js
@@ -4,12 +4,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const routes = require('./routes/lookupmusicbandRoutes');
 
 const app = express();
-app.use(cors());
 
 const {
   LOOKUPMUSICBAND_PORT,
